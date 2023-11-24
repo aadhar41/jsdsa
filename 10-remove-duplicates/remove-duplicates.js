6cfc9750772d72391fdd474110d0e47e4d7757e1
Aadhar gaur <aadhar41@gmail.com>
2023-11-24 13:41:16 +0530

    10-remove-duplicates Updated

--- 10-remove-duplicates/remove-duplicates.js
+++ 10-remove-duplicates/remove-duplicates.js
@@ -1,13 +1,18 @@
-function removeDuplicates(array) {
-    const uniqueArr = [];
+// function removeDuplicates(array) {
+//     const uniqueArr = [];
 
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        if (!uniqueArr.includes(element)) {
-            uniqueArr.push(element);
-        }
-    }
-    return uniqueArr;
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         if (!uniqueArr.includes(element)) {
+//             uniqueArr.push(element);
+//         }
+//     }
+//     return uniqueArr;
+// }
+
+
+function removeDuplicates(array) {
+    return Array.from(new Set(array));
 }
 
 module.exports = removeDuplicates;
